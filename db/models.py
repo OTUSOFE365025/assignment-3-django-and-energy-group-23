db2b285ccaba8ed483601a5ed5f7e3c4a294cae3
Yanexgram <yanex1440@gmail.com>
2025-11-10 22:24:43 -0500

    Remove User model and Django import error handling
    Removed the User model and error handling for Django import.

--- db/models.py
+++ db/models.py
@@ -1,16 +1,1 @@
-import sys
-
-try:
-    from django.db import models
-except Exception:
-    print('Exception: Django Not Found, please install it with "pip install django".')
-    sys.exit()
-
-
-# Sample User model
-class User(models.Model):
-    name = models.CharField(max_length=50, default="Dan")
-
-    def __str__(self):
-        return self.name
 
