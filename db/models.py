52f2584c76d5ba4e1420a11b72eb1d7ae5211a1f
Yanexgram <yanex1440@gmail.com>
2025-11-10 22:28:22 -0500

    updated model.py

--- db/models.py
+++ db/models.py
@@ -1,1 +1,21 @@
+import sys
+try:
+    from django.db import models
+except Exception:
+    print('Django not installed. Run: pip install django')
+    sys.exit()
+
+class User(models.Model):
+    name = models.CharField(max_length=50, default="Dan")
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    upc = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
 
